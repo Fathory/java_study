81ec1f368375e0ff12443958f03b04a07705cbc1
fathory <ggje33@gmail.com>
2019-02-15 09:58:03 +0900

    4,5단까지 // 사실은 전부 다 구현됨

--- src/googoodan/googoodan.java
+++ src/googoodan/googoodan.java
@@ -1,7 +1,12 @@
 package googoodan;
 
+import java.util.Scanner;
+
 class TwoThree{
 	public static void main(String[] args) {
+		// 2단
+		System.out.println("2단");
+		
 		System.out.println(2*1);
 		System.out.println(2*2);
 		System.out.println(2*3);
@@ -11,6 +16,10 @@ class TwoThree{
 		System.out.println(2*7);
 		System.out.println(2*8);
 		System.out.println(2*9);
+		
+		// 3단
+		System.out.println("3단");
+		
 		System.out.println(3*1);
 		System.out.println(3*2);
 		System.out.println(3*3);
@@ -20,5 +29,32 @@ class TwoThree{
 		System.out.println(3*7);
 		System.out.println(3*8);
 		System.out.println(3*9);
+		
+		// Scanner Java Util Add
+		// 콘솔에서 값을 입력받을 수 있는 유틸
+		// 4, 5단
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		int result = 0;
+		scan.close(); // 입력받은 후에는 닫아주는 것이 좋음.
+		System.out.println(i+"단");
+		result = i*1;
+		System.out.println(result);
+		result = i*2;
+		System.out.println(result);
+		result = i*3;
+		System.out.println(result);
+		result = i*4;
+		System.out.println(result);
+		result = i*5;
+		System.out.println(result);
+		result = i*6;
+		System.out.println(result);
+		result = i*7;
+		System.out.println(result);
+		result = i*8;
+		System.out.println(result);
+		result = i*9;
+		System.out.println(result);
 	}
 }
